bbaf4616f3a62915fffc1548683e57d4165927e1
francisjosephironman <francis.3.6.joseph@gmail.com>
2023-01-21 12:52:40 +0800

    created table to display all todos

--- simple-todo-list-frontend/src/app/todo-list/todo-list.component.ts
+++ simple-todo-list-frontend/src/app/todo-list/todo-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  
+  todos?: Todo[];
 
+  ngOnInit(): void {
+    this.todos = [
+      {
+        "id": 1,
+        "title": "testing",
+        "description": "test description"
+      },
+      {
+        "id": 2,
+        "title": "testing",
+        "description": "test description"
+      },
+      {
+        "id": 3,
+        "title": "testing",
+        "description": "test description"
+      },
+    ]
+  }
 }
